504b7060d4843107d2227ab59ca4876f8cf75142
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2020-04-01 14:18:57 +0200

    Added utils component for data transforming, path normalizing, etc.

---
+++ src/test/mock/utils.mock.ts
@@ -0,0 +1,58 @@
+import * as vscode from "vscode";
+import QuickPickItem from "../../interface/quickPickItem";
+
+export const qpItem: QuickPickItem = {
+  label: "fake-1.ts",
+  uri: vscode.Uri.file("./fake/fake-1.ts"),
+  symbolKind: 0
+};
+
+const qpItems: QuickPickItem[] = [
+  {
+    label: "fake-1.ts",
+    description: "File",
+    detail: "\\.\\fake\\fake-1.ts",
+    uri: vscode.Uri.file("./fake/fake-1.ts"),
+    symbolKind: 0,
+    range: {
+      start: new vscode.Position(0, 0),
+      end: new vscode.Position(0, 0)
+    }
+  },
+  {
+    label: "fake-2.ts",
+    description: "File",
+    detail: "\\.\\fake\\fake-2.ts",
+    uri: vscode.Uri.file("./fake/fake-2.ts"),
+    symbolKind: 0,
+    range: {
+      start: new vscode.Position(0, 0),
+      end: new vscode.Position(0, 0)
+    }
+  }
+];
+
+qpItems.forEach((qpItem: any) => (qpItem.uri._fsPath = qpItem.detail));
+export { qpItems };
+
+export const item: vscode.Uri = vscode.Uri.file(
+  "./test/path/to/workspace/fake/fake-1.ts"
+);
+
+export const items: vscode.Uri[] = [
+  "./fake/fake-1.ts",
+  "./fake/fake-2.ts"
+].map((path: string) => vscode.Uri.file(path));
+
+export const workspaceFolders: vscode.WorkspaceFolder[] = [
+  {
+    index: 0,
+    name: "/test/path/to/workspace",
+    uri: vscode.Uri.file("/test/path/to/workspace")
+  },
+  {
+    index: 1,
+    name: "/test2/path2/to2/workspace2",
+    uri: vscode.Uri.file("/test2/path2/to2/workspace2")
+  }
+];
---
+++ src/test/test/utils.test.ts
@@ -0,0 +1,61 @@
+import * as vscode from "vscode";
+import { assert } from "chai";
+import * as sinon from "sinon";
+import Utils from "../../utils";
+import * as mock from "../mock/utils.mock";
+import QuickPick from "../../interface/quickPickItem";
+
+describe("Utis", () => {
+  let utils: Utils;
+  let utilsAny: any;
+
+  before(() => {
+    utils = new Utils();
+  });
+
+  beforeEach(() => {
+    utilsAny = utils as any;
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe("prepareQpData", () => {
+    it("should return quick pick data", () => {
+      assert.deepEqual(utils.prepareQpData(mock.items), mock.qpItems);
+    });
+
+    it("should return empty array", () => {
+      assert.deepEqual(utils.prepareQpData([]), []);
+    });
+  });
+
+  describe("normalizeUriPath", () => {
+    it("should return uri path without workspace part", () => {
+      sinon
+        .stub(vscode.workspace, "workspaceFolders")
+        .value(mock.workspaceFolders);
+
+      assert.equal(
+        utilsAny.normalizeUriPath(mock.item.fsPath),
+        mock.qpItem.uri!.fsPath
+      );
+    });
+  });
+
+  describe("getWorkspaceFoldersPaths", () => {
+    it("should return array with two strings", () => {
+      sinon
+        .stub(vscode.workspace, "workspaceFolders")
+        .value(mock.workspaceFolders);
+      const workspaceFoldersPaths = utilsAny.getWorkspaceFoldersPaths();
+      const expected = [
+        "\\test\\path\\to\\workspace",
+        "\\test2\\path2\\to2\\workspace2"
+      ];
+
+      assert.deepEqual(workspaceFoldersPaths, expected);
+    });
+  });
+});
