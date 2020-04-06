65cb63463ff41f93df269236460ea5b1cc3c26b5
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2020-04-06 14:18:25 +0200

    Usage of exclude and include patterns from configuration instead of hardcoded ones

--- src/dataService.ts
+++ src/dataService.ts
@@ -1,12 +1,41 @@
 import * as vscode from "vscode";
+import Config from "./config";
 
 class DataService {
+  private config: Config;
+
+  constructor() {
+    this.config = new Config();
+  }
+
   async getData(): Promise<vscode.Uri[]> {
+    const includePatterns = this.getIncludePatterns();
+    const excludePatterns = this.getExcludePatterns();
     const files = await vscode.workspace.findFiles(
-      "**/*",
-      "**/node_modules/**"
+      includePatterns,
+      excludePatterns
     );
     return files;
+  }
+
+  private getIncludePatterns(): string {
+    const includePatterns = this.config.getInclude();
+    return this.patternsAsString(includePatterns);
+  }
+
+  private getExcludePatterns(): string {
+    const excludePatterns = this.config.getExclude();
+    return this.patternsAsString(excludePatterns);
+  }
+
+  private patternsAsString(patterns: string[]): string {
+    if (patterns.length === 0) {
+      return "";
+    } else if (patterns.length === 1) {
+      return patterns[0];
+    } else {
+      return `{${patterns.join(",")}}`;
+    }
   }
 }
 
--- src/test/test/dataService.test.ts
+++ src/test/test/dataService.test.ts
@@ -6,9 +6,14 @@ import * as mock from "../mock/dataService.mock";
 
 describe("DataService", () => {
   let dataService: DataService;
+  let dataServiceAny: any;
 
   before(() => {
     dataService = new DataService();
+  });
+
+  beforeEach(() => {
+    dataServiceAny = dataService as any;
   });
 
   afterEach(() => {
@@ -31,6 +36,59 @@ describe("DataService", () => {
       const items = await dataService.getData();
 
       assert.equal(items.length, 2);
+    });
+  });
+
+  describe("getIncludePatterns", () => {
+    it("should return string[] containing include patterns", () => {
+      const patterns = ["**/*.{js}"];
+      sinon.stub(dataServiceAny.config, "getInclude").returns(patterns);
+
+      assert.equal(dataServiceAny.getIncludePatterns(), patterns);
+    });
+  });
+
+  describe("getExcludePatterns", () => {
+    it("should return string[] containing exclude patterns", () => {
+      const patterns = ["**/node_modules/**"];
+      sinon.stub(dataServiceAny.config, "getExclude").returns(patterns);
+
+      assert.equal(dataServiceAny.getExcludePatterns(), patterns);
+    });
+  });
+
+  describe("patternsAsString", () => {
+    it("should return empty string if passed array is empty", () => {
+      const patterns: string[] = [];
+
+      assert.equal(
+        dataServiceAny.patternsAsString(patterns),
+        patterns.join(",")
+      );
+    });
+
+    it(`should return string with one pattern if passed array
+      contains one element`, () => {
+      const patterns = ["**/node_modules/**"];
+
+      assert.equal(
+        dataServiceAny.patternsAsString(patterns),
+        patterns.join(",")
+      );
+    });
+
+    it(`should return string with patterns separated by comma surrounded
+      with curly braces if passed array contains more than one element`, () => {
+      const patterns = [
+        "**/node_modules/**",
+        "**/bower_components/**",
+        "**/yarn.lock",
+      ];
+
+      assert.equal(
+        dataServiceAny.patternsAsString(patterns),
+        `{${patterns.join(",")}}`
+      );
     });
   });
 });
