84552ece6d5957a94493d6b37ef2e0555f9dca46
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2022-08-09 14:43:17 +0200

    refactor: improve extension test code quality and readability

--- src/test/test/extension.test.ts
+++ src/test/test/extension.test.ts
@@ -1,62 +1,54 @@
-// import { assert } from "chai";
-// import * as sinon from "sinon";
-// import * as vscode from "vscode";
-// import * as extension from "../../extension";
-// import ExtensionController from "../../extensionController";
-// import { getTestSetups } from "../testSetup/extension.testSetup";
-// import { getExtensionContext } from "../util/mockFactory";
+import { assert } from "chai";
+import * as sinon from "sinon";
+import * as vscode from "vscode";
+import * as extension from "../../extension";
+import { getTestSetups } from "../testSetup/extension.testSetup";
 
-// describe("extension", () => {
-//   let context: vscode.ExtensionContext = getExtensionContext();
-//   let extensionController: ExtensionController = new ExtensionController(
-//     context
-//   );
-//   let setups = getTestSetups(extensionController);
+type SetupsType = ReturnType<typeof getTestSetups>;
 
-//   beforeEach(() => {
-//     context = getExtensionContext();
-//     extensionController = new ExtensionController(context);
-//     setups = getTestSetups(extensionController);
-//   });
+describe("extension", () => {
+  let setups: SetupsType;
+  let context: vscode.ExtensionContext;
 
-//   describe("activate", () => {
-//     it("1: should register two commands", async () => {
-//       const [registerCommandStub] = setups.activate1();
-//       await extension.activate(context);
+  before(() => {
+    setups = getTestSetups();
+    context = setups.before();
+  });
+  afterEach(() => setups.afterEach());
 
-//       assert.equal(registerCommandStub.calledTwice, true);
-//     });
-//   });
+  describe("activate", () => {
+    it("1: should register two commands", async () => {
+      const [registerCommandStub] = setups.activate1();
+      await extension.activate(context);
 
-//   describe("deactivate", () => {
-//     it("1: should function exist", () => {
-//       // const sandbox = sinon.createSandbox();
-//       // const stub = sandbox.stub();
-//       // const logStub = sandbox.replaceGetter(console, "log", () => stub());
-//       const logStub = sinon.spy(console, "log", ["get"]);
-//       // const [logStub] = setups.deactivate1();
-//       // const logSpy = sinon.spy(console, "log", ["get"]);
-//       extension.deactivate();
+      assert.equal(registerCommandStub.calledTwice, true);
+    });
+  });
 
-//       assert.equal(logStub.get.calledOnce, true);
-//     });
-//   });
+  describe("deactivate", () => {
+    it("1: should function exist", () => {
+      const logStub = sinon.spy(console, "log", ["get"]);
+      extension.deactivate();
 
-//   describe("search", () => {
-//     it("1: should extensionController.search method be invoked", () => {
-//       const [searchStub] = setups.search1();
-//       extension.search(extensionController);
+      assert.equal(logStub.get.calledOnce, true);
+    });
+  });
 
-//       assert.equal(searchStub.calledOnce, true);
-//     });
-//   });
+  describe("search", () => {
+    it("1: should extensionController.search method be invoked", () => {
+      const [searchStub] = setups.search1();
+      extension.search();
 
-//   describe("reload", () => {
-//     it("1: should extensionController.reload method be invoked", () => {
-//       const [reloadStub] = setups.reload1();
-//       extension.reload(extensionController);
+      assert.equal(searchStub.calledOnce, true);
+    });
+  });
 
-//       assert.equal(reloadStub.calledOnce, true);
-//     });
-//   });
-// });
+  describe("reload", () => {
+    it("1: should extensionController.reload method be invoked", () => {
+      const [reloadStub] = setups.reload1();
+      extension.reload();
+
+      assert.equal(reloadStub.calledOnce, true);
+    });
+  });
+});
--- src/test/testSetup/extension.testSetup.ts
+++ src/test/testSetup/extension.testSetup.ts
@@ -1,22 +1,40 @@
+import * as sinon from "sinon";
 import * as vscode from "vscode";
 import { extensionController } from "../../extensionController";
+import { getExtensionContext } from "../util/mockFactory";
 import { stubMultiple } from "../util/stubHelpers";
 
 export const getTestSetups = () => {
+  const sandbox = sinon.createSandbox();
+  const context = getExtensionContext();
+
   return {
+    before: () => {
+      return context;
+    },
+    afterEach: () => {
+      sandbox.restore();
+    },
     activate1: () => {
-      return stubMultiple([
-        { object: vscode.commands, method: "registerCommand" },
-      ]);
+      return stubMultiple(
+        [{ object: vscode.commands, method: "registerCommand" }],
+        sandbox
+      );
     },
     deactivate1: () => {
-      return stubMultiple([{ object: console, method: "log" }]);
+      return stubMultiple([{ object: console, method: "log" }], sandbox);
     },
     search1: () => {
-      return stubMultiple([{ object: extensionController, method: "search" }]);
+      return stubMultiple(
+        [{ object: extensionController, method: "search" }],
+        sandbox
+      );
     },
     reload1: () => {
-      return stubMultiple([{ object: extensionController, method: "reload" }]);
+      return stubMultiple(
+        [{ object: extensionController, method: "reload" }],
+        sandbox
+      );
     },
   };
 };
