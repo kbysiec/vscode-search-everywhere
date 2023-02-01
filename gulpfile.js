9b63a2c9f93df0b4078ac2c052f019b4d7f41073
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2023-02-01 12:04:19 +0100

    refactor: remove redundant console.log

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,14 @@ const gulp = require("gulp");
 const replacer = require("gulp-string-replacer");
 
 function fixCoverageReportSrcPaths() {
-  console.log("*** gulp task called");
   return gulp
     .src("./dist/coverage/coverage-final.json")
     .pipe(
       replacer([
         {
           regex: /(path\":)(.*)(?=src)/,
-          replacement: `path":"`
-        }
+          replacement: `path":"`,
+        },
       ])
     )
     .pipe(gulp.dest("./dist/coverage"));
